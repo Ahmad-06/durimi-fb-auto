be364ff19595a946546ee03dbf9fe999ab5af8e0
Ahmad Shahzad <ahmad@shahzad.dev>
2023-09-07 15:57:29 +0500

    Validate: media.

--- router/api/v1/posts/routes/posts.create.js
+++ router/api/v1/posts/routes/posts.create.js
@@ -5,7 +5,7 @@ module.exports = CREATE;
 
 const openDB = require('../../../../../data/openDB');
 
-const { isValidURL } = require('../../../../../utils/utils');
+const { isValidURL, isJSONParsable } = require('../../../../../utils/utils');
 
 CREATE.post('/', async (req, res) => {
     // Create a database connection.
@@ -15,6 +15,7 @@ CREATE.post('/', async (req, res) => {
     const type = req?.body?.type ? req?.body?.type : null;
     const message = req?.body?.message ? req?.body?.message : null;
     const link = req?.body?.link ? req?.body?.link : null;
+    const media = req?.body?.media ? req?.body?.media : null;
 
     /**
      * INPUT VALIDATION.
@@ -48,6 +49,20 @@ CREATE.post('/', async (req, res) => {
                 moment: 'Validating link submitted by the user.',
                 message:
                     "The link you submitted is invalid. Make sure it's of the following format: http(s)://(www.)example.org",
+            },
+        });
+
+    // Verify the media.
+    if (media !== null && !isJSONParsable(media))
+        return res.status(400).json({
+            success: false,
+            data: null,
+            error: {
+                code: 400,
+                type: 'Invalid user input.',
+                route: '/api/v1/posts/create',
+                moment: 'Validating media submitted by the user.',
+                message: "The media you submitted is invalid. Make sure it's a stringified array of Base64 Image URLs.",
             },
         });
 
