8f3c8758e3fba969524b8faafb057d02f9dd40c9
Ahmad Shahzad <ahmad@shahzad.dev>
2023-09-08 14:54:32 +0500

    Created a route to get posts of either type.

--- router/api/v1/posts/routes/posts.get.js
+++ router/api/v1/posts/routes/posts.get.js
@@ -41,3 +41,60 @@ GET.get('/all', async (req, res) => {
         }
     }
 });
+
+GET.get('/:type', async (req, res) => {
+    // Connect to the database.
+    const db = await openDB();
+
+    const type = req.params.type;
+    const types = ['automated', 'scheduled'];
+
+    if (!types.includes(type)) {
+        await db.close();
+
+        return res.status(404).json({
+            success: false,
+            data: null,
+            error: {
+                code: 404,
+                type: 'Resource not found.',
+                route: `/api/v1/posts/${type}`,
+                moment: `Trying to get posts of type '${type}' from the database.`,
+                message: 'No such route or post type exists.',
+            },
+        });
+    }
+
+    try {
+        const query = 'SELECT * FROM posts WHERE type = ?';
+        const params = [type];
+
+        const posts = await db.all(query, params);
+
+        res.json({
+            success: true,
+            data: {
+                posts,
+            },
+            error: null,
+        });
+
+        await db.close();
+    } catch (err) {
+        if (err) {
+            await db.close();
+
+            return res.status(500).json({
+                success: false,
+                data: null,
+                error: {
+                    code: 500,
+                    type: 'Internal server error.',
+                    route: `/api/v1/posts/${type}`,
+                    moment: `Echoing all posts of '${type}' type from the database.`,
+                    message: err.toString(),
+                },
+            });
+        }
+    }
+});
