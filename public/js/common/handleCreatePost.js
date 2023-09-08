ac6cec530b4651326ff48ca9442871eeff9c54e0
Ahmad Shahzad <ahmad@shahzad.dev>
2023-09-08 19:30:20 +0500

    Create new post handler.

---
+++ public/js/common/handleCreatePost.js
@@ -0,0 +1,32 @@
+const createPost = async (index, type) => {
+    const apiType = index === '0' ? 'create' : 'update';
+    const apiEndpoint = type === 'publish' ? api.posts.publish : api.posts[apiType];
+
+    const post = {
+        id: getInput('id', index).value,
+        message: getInput('message', index).value,
+        link: getInput('link', index).value,
+        media: getInput('media', index).value,
+        context: getInput('context', index).value,
+        publisher: getInput('publisher', index).value,
+        time: getInput('time', index).value,
+        type,
+    };
+
+    const resp = await fetch(apiEndpoint, {
+        method: apiType === 'update' ? 'PUT' : 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(post),
+    });
+    const { success, error } = await resp.json();
+
+    if (!success) return handleError('Error encountered when trying to create the Post.', error);
+
+    window.location.href = '/posts';
+};
+
+const getInput = (name, index) => {
+    return document.querySelector(`[name="${name}"][data-form-id="${index}"]`);
+};
