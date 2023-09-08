825b627aaaafc7deb33c38d136e687c44590cc79
Ahmad Shahzad <ahmad@shahzad.dev>
2023-09-08 14:49:09 +0500

    Created a route to get all posts.

--- router/api/v1/posts/routes/posts.get.js
+++ router/api/v1/posts/routes/posts.get.js
@@ -2,3 +2,42 @@ const express = require('express');
 
 const GET = express.Router();
 module.exports = GET;
+
+const openDB = require('../../../../../data/openDB');
+
+GET.get('/all', async (req, res) => {
+    // Connect to the database.
+    const db = await openDB();
+
+    try {
+        const query = 'SELECT * FROM posts';
+
+        const posts = await db.all(query);
+
+        res.json({
+            success: true,
+            data: {
+                posts,
+            },
+            error: null,
+        });
+
+        await db.close();
+    } catch (err) {
+        if (err) {
+            await db.close();
+
+            return res.status(500).json({
+                success: false,
+                data: null,
+                error: {
+                    code: 500,
+                    type: 'Internal server error.',
+                    route: '/api/v1/posts/all',
+                    moment: 'Echoing all posts from the database.',
+                    message: err.toString(),
+                },
+            });
+        }
+    }
+});
