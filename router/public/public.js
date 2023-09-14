aec8d74e2b7805f0d9009f94e1391b93190a9218
Ahmad Shahzad <ahmad@shahzad.dev>
2023-09-14 22:16:06 +0500

    Created dummy auth route handler.

--- router/public/public.js
+++ router/public/public.js
@@ -4,5 +4,6 @@ const public = express.Router();
 module.exports = public;
 
 public.use('/', require('./home/home'));
+public.use('/auth', require('./auth/auth'));
 public.use('/posts', require('./posts/posts'));
 public.use('/timesheet', require('./timesheet/timesheet'));
