02e6ba305af31a0d51b18f152f9bd380ddc558e0
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-17 10:56:51 +0500

    Handling the groups in post creation route.

--- public/js/common/handleCreatePost.js
+++ public/js/common/handleCreatePost.js
@@ -2,12 +2,20 @@ const createPost = async (index, type) => {
     const apiType = index === '0' ? 'create' : 'update';
     const apiEndpoint = type === 'publish' ? api.posts.publish : api.posts[apiType];
 
+    let groups = [];
+    const groupInputs = document.querySelectorAll('input[name="groups"]:checked');
+
+    for (let i = 0; i < groupInputs.length; i++) {
+        groups.push(groupInputs[i].value);
+    }
+
     const post = {
         id: getInput('id', index).value,
         message: getInput('message', index).value,
         link: getInput('link', index).value,
         media: getInput('media', index).value,
         images: getInput('images', index).value,
+        groups,
         context: getInput('context', index).value,
         publisher: getInput('publisher', index).value,
         time: getInput('time', index).value,
