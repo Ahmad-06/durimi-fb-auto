fbc2f36e89d018aab93dac0096ca91e754898746
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-17 08:59:54 +0500

    Created group-related api endpoints.

---
+++ router/api/v1/groups/routes/groups.get.js
@@ -0,0 +1,4 @@
+const express = require('express');
+
+const GET = express.Router();
+module.exports = GET;
--- router/api/v1/v1.js
+++ router/api/v1/v1.js
@@ -5,4 +5,5 @@ module.exports = v1;
 
 v1.use('/misc', require('./misc/misc'));
 v1.use('/posts', require('./posts/posts'));
+v1.use('/groups', require('./groups/groups'));
 v1.use('/timesheet', require('./timesheet/timesheet'));
