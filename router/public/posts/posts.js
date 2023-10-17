bc3e97e7bb6d119ed377c3ebab96b2ddf4924ba8
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-17 12:17:29 +0500

    Groups on posts page.

--- router/public/posts/posts.js
+++ router/public/posts/posts.js
@@ -11,12 +11,49 @@ const changePass = require('../../../utils/changePass');
 posts.get('/', loggedIn, changePass, async (req, res) => {
     const db = await openDB();
 
-    let automated, scheduled;
+    let groups, automated, scheduled;
+
+    try {
+        const query = 'SELECT * FROM groups;';
+
+        groups = await db.all(query);
+
+        let new_groups = {};
+
+        for (let i = 0; i < groups.length; i++) {
+            new_groups[groups[i].id] = groups[i].name;
+        }
+
+        groups = new_groups;
+    } catch (err) {
+        if (err) {
+            await db.close();
+
+            return res.render('errorpage', {
+                message: 'There was an error trying to get the groups from the database.',
+            });
+        }
+    }
 
     try {
         const query = "SELECT * FROM posts WHERE type = 'automated' ORDER BY priority;";
 
         automated = await db.all(query);
+
+        for (let i = 0; i < automated.length; i++) {
+            if (automated[i].groups !== 'null') {
+                let post_groups = JSON.parse(automated[i].groups);
+                let post_groups_new = [];
+
+                for (let j = 0; j < post_groups.length; j++) {
+                    post_groups_new.push({ id: post_groups[j], name: groups[post_groups[j]] });
+                }
+
+                post_groups = post_groups_new;
+
+                automated[i].groups = post_groups;
+            }
+        }
     } catch (err) {
         if (err) {
             await db.close();
@@ -31,6 +68,21 @@ posts.get('/', loggedIn, changePass, async (req, res) => {
         const query = "SELECT * FROM posts WHERE type = 'scheduled' ORDER BY priority;";
 
         scheduled = await db.all(query);
+
+        for (let i = 0; i < scheduled.length; i++) {
+            if (scheduled[i].groups !== 'null') {
+                let post_groups = JSON.parse(scheduled[i].groups);
+                let post_groups_new = [];
+
+                for (let j = 0; j < post_groups.length; j++) {
+                    post_groups_new.push({ id: post_groups[j], name: groups[post_groups[j]] });
+                }
+
+                post_groups = post_groups_new;
+
+                scheduled[i].groups = post_groups;
+            }
+        }
     } catch (err) {
         if (err) {
             await db.close();
