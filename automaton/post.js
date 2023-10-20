7647a6102ea0d6aae2930f09d2ead8f501cbcc9e
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-20 23:35:56 +0500

    Enabled headless mode.

--- automaton/post.js
+++ automaton/post.js
@@ -5,7 +5,7 @@ const { sleep } = require('../utils/utils');
 
 module.exports = async (post, auth) => {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: 'new',
         defaultViewport: null,
         args: ['--start-maximized', '--disable-notifications'],
         userDataDir: path.join(__dirname, 'userData'),
