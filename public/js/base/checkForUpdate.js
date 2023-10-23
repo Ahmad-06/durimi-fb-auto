c8ddbe684c4d87faf1e76e758aa1be020a5ddf0a
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-23 09:10:55 +0500

    Checking for the cookie before calling the api.

--- public/js/base/checkForUpdate.js
+++ public/js/base/checkForUpdate.js
@@ -1,12 +1,13 @@
 (async () => {
-    const apiEndpoint = api.misc.update;
-    const resp = await fetch(apiEndpoint);
-    const { success, data, error } = await resp.json();
+    const allCookies = document.cookie;
+    const cookies = allCookies.split(';');
 
-    if (success) {
-        const allCookies = document.cookie;
-        const cookies = allCookies.split(';');
-        if (!cookies.includes('checkUpdate=false')) {
+    if (!cookies.includes('checkUpdates=false')) {
+        const apiEndpoint = api.misc.update;
+        const resp = await fetch(apiEndpoint);
+        const { success, data, error } = await resp.json();
+
+        if (success) {
             if (data.update) {
                 const body = document.querySelector('body');
                 const updateIndicator = document.getElementById('update-indicator');
@@ -14,8 +15,8 @@
                 body.classList.add('update-indicator-visible');
                 updateIndicator.classList.remove('hidden');
             }
+        } else {
+            handleError('Server did not respond with the requested update data.', error);
         }
-    } else {
-        handleError('Server did not respond with the requested update data.', error);
     }
 })();
