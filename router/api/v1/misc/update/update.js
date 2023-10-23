f76a4efff2a86c006c2a38b2e5280b30d24b7555
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-23 07:15:03 +0500

    Created the logic for the misc/update endpoint.

--- router/api/v1/misc/update/update.js
+++ router/api/v1/misc/update/update.js
@@ -3,10 +3,32 @@ const express = require('express');
 const update = express.Router();
 module.exports = update;
 
-update.get('/', (req, res) => {
-    res.json({
-        success: true,
-        data: null,
-        error: null,
-    });
+const getUpdate = require('../../../../../utils/update');
+
+update.get('/', async (req, res) => {
+    try {
+        const canUpdate = await getUpdate();
+
+        res.json({
+            success: true,
+            data: {
+                update: canUpdate,
+            },
+            error: null,
+        });
+    } catch (err) {
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                data: null,
+                error: {
+                    code: 500,
+                    type: 'Internal server error',
+                    route: '/api/v1/misc/update',
+                    moment: 'Checking if a new update is available.',
+                    message: err.toString(),
+                },
+            });
+        }
+    }
 });
