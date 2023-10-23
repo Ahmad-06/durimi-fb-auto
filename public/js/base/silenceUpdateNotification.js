025cdfa8c21fdaefeab114a974a24b9fe3b15111
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-23 08:06:36 +0500

    Set a cookie that prevents checking for updates for another 12 hours.

--- public/js/base/silenceUpdateNotification.js
+++ public/js/base/silenceUpdateNotification.js
@@ -4,4 +4,9 @@ const silenceUpdateNotification = () => {
 
     updateIndicator.classList.add('hidden');
     body.classList.remove('update-indicator-visible');
+
+    const currentTime = new Date().getTime();
+    const futureTime = new Date(currentTime + 1000 * 60 * 60 * 12).toUTCString();
+
+    document.cookie = `checkUpdate=false; expires=${futureTime}; path=/`;
 };
