e019d3e53dbe1c1a1306b56585429fae0890de32
Ahmad Shahzad <ahmad@shahzad.dev>
2023-10-24 18:00:05 +0500

    Created the logic to add timeslots to all days at once.

--- public/js/timesheet/handleInstantTimesheet.js
+++ public/js/timesheet/handleInstantTimesheet.js
@@ -17,3 +17,41 @@
         }
     });
 })();
+
+const createInstantTimeslots = async () => {
+    const apiEndpoint = api.timesheet.instant;
+
+    const allDays = document.getElementById('modal-instant-days-all');
+    let checkedDays = [];
+
+    if (allDays.checked) {
+        checkedDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    } else {
+        const daysChecked = document.querySelectorAll('.modal-instant-checkbox:checked');
+
+        for (let i = 0; i < daysChecked.length; i++) {
+            const dayChecked = daysChecked[i];
+
+            checkedDays.push(dayChecked.value);
+        }
+    }
+
+    const time = document.getElementById('modal-instant-time').value;
+
+    const timesheet = { days: checkedDays, time };
+
+    const resp = await fetch(apiEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(timesheet),
+    });
+    const { success, error } = await resp.json();
+
+    if (!success) return handleError('There was an error when creating the timeslots in the database.', error);
+
+    if (success && error) return handleError('There was an error when creating the timeslots in the database.', error);
+
+    window.location.href = '/timesheet';
+};
--- router/api/v1/timesheet/timesheet.js
+++ router/api/v1/timesheet/timesheet.js
@@ -7,3 +7,4 @@ timesheet.use('/', require('./routes/timesheet.get'));
 timesheet.use('/create', require('./routes/timesheet.create'));
 timesheet.use('/update', require('./routes/timesheet.update'));
 timesheet.use('/delete', require('./routes/timesheet.delete'));
+timesheet.use('/instant', require('./routes/timesheet.instant'));
