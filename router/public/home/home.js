b7b9b16ae7c3a723188c2dcadb5248038c22bed4
Ahmad Shahzad <146444891+Ahmad-06@users.noreply.github.com>
2023-10-24 18:10:13 +0500

    feat: env flag to select default context/publisher. (#14)

--- router/public/home/home.js
+++ router/public/home/home.js
@@ -8,6 +8,8 @@ const openDB = require('../../../data/openDB');
 const { loggedIn } = require('../../../utils/loggedIn');
 const changePass = require('../../../utils/changePass');
 
+const [context, publisher] = (process.env.CONPUB || 'page-page').split('-');
+
 home.get('/', loggedIn, changePass, async (req, res) => {
     const db = await openDB();
 
@@ -26,7 +28,11 @@ home.get('/', loggedIn, changePass, async (req, res) => {
         }
     }
 
-    res.render('home/home', { groups });
+    res.render('home/home', {
+        groups,
+        context: ['page', 'group'].includes(context) ? context : 'page',
+        publisher: ['page', 'user'].includes(publisher) ? publisher : 'page',
+    });
 
     await db.close();
 });
