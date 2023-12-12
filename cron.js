0cad3bbd581732bcdbb88898eed486f88e5ad620
Ahmad Shahzad <146444891+Ahmad-06@users.noreply.github.com>
2023-12-12 21:10:58 +0000

    fix: disabled the cron for meta-business-suite json.

--- cron.js
+++ cron.js
@@ -8,20 +8,20 @@ const openDB = require('./data/openDB');
 const pubsub = require('./utils/pubsub');
 
 // Fetch the endpoints from the remote repository every 30 minutes.
-cron.schedule('*/30 * * * *', async () => {
-    const metaEndpoint = 'https://airy-beaded-caravel.glitch.me/meta-business-suite.json';
-    const resp = await fetch(metaEndpoint);
-    if (resp.ok) {
-        const data = await resp.json();
+// cron.schedule('*/30 * * * *', async () => {
+//     const metaEndpoint = 'https://airy-beaded-caravel.glitch.me/meta-business-suite.json';
+//     const resp = await fetch(metaEndpoint);
+//     if (resp.ok) {
+//         const data = await resp.json();
 
-        if (data?.meta?.selector?.success) {
-            const selectors = JSON.stringify(data);
-            const selectorsPath = path.join(__dirname, 'data', 'meta-business-suite.json');
-            const currentSelectors = JSON.parse(fs.readFileSync(selectorsPath));
-            if (currentSelectors.updated !== data.updated) fs.writeFileSync(selectorsPath, selectors);
-        }
-    }
-});
+//         if (data?.meta?.selector?.success) {
+//             const selectors = JSON.stringify(data);
+//             const selectorsPath = path.join(__dirname, 'data', 'meta-business-suite.json');
+//             const currentSelectors = JSON.parse(fs.readFileSync(selectorsPath));
+//             if (currentSelectors.updated !== data.updated) fs.writeFileSync(selectorsPath, selectors);
+//         }
+//     }
+// });
 
 // The cronjob for scheduled posts.
 cron.schedule('*/5 * * * *', async () => {
